5ea2312d702b1e0a6e54d25a1d870b1ef66cc886
Hartik <rathodhartik@gmail.com>
2021-12-28 16:16:16 +0530

    mrege submain1

--- app/models.py
+++ app/models.py
@@ -5,11 +5,8 @@ class Student(models.Model):
     name=models.CharField(max_length=100)
     address=models.TextField()
     age=models.IntegerField()
-<<<<<<< HEAD
-=======
     subject=models.CharField(max_length=100)
     
->>>>>>> hd
 
     marks=models.IntegerField()
     
